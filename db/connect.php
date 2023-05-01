3001ebc65c8306066676c4feb1e271591fbdd74d
Dat Le Thac <datquynhvinh1231@gmail.com>
2023-05-01 12:44:07 +0700

    update

--- db/connect.php
+++ db/connect.php
@@ -1,10 +1,5 @@
 <?php
-$con = mysqli_connect(
-  DB_HOST,
-  DB_USER,
-  DB_PASS,
-  DB_NAME
-);
+$con = mysqli_connect("localhost","root","","izfaqdfz_webdoan");
 
 // Check connection
 if (mysqli_connect_errno())
